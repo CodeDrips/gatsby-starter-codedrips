ac3e4e4aa19e37d832ea0db27c2fa0efcf6ae1e7
Will Pringle <hello@will-pringle.com>
2021-06-02 15:02:34 +1000

    add gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
