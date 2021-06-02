35b40b3e395f5b8b9c748c2aea73a0f75a35edcb
Will Pringle <hello@will-pringle.com>
2021-06-02 11:58:41 +1000

    add filesystem plugin back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `codedrips`,
